83baafb4a984bf01ff88b698a6589ae89bda4625
ja1rdev <jairenriquez1715@gmail.com>
2025-09-04 12:15:17 -0500

    improved sketch_sep3a

--- esp32_sketchs/examples/sketch_sep3a/sketch_sep3a.ino
+++ esp32_sketchs/examples/sketch_sep3a/sketch_sep3a.ino
@@ -1,64 +1,109 @@
-// C++ code
-char opt; // Variable public
-int const LEDRED = 4;
-int const LEDGREEN = 1;
-int const LEDYELLOW = 2;
+// LED pins
+const int LEDRED = 4;
+const int LEDGREEN = 1;
+const int LEDYELLOW = 2;
 
 void setup() {
+    // Config the pins as output
     pinMode(LEDRED, OUTPUT);
     pinMode(LEDYELLOW, OUTPUT);
     pinMode(LEDGREEN, OUTPUT);
-    Serial.begin(9600); // Baudios
-    Serial.println("WELCOME");
-    Serial.println("Press any key to continue");
+    
+    // Initialize serial
+    Serial.begin(9600);
+    
+    // Show main menu
+    mainMenu();
 }
+
 void loop() {
     if (Serial.available() > 0) {
-        mainMenu();
-        opt = Serial.read();
-        if (opt == '1') {
-            digitalWrite(LEDRED, HIGH);
-        } else if (opt == '2') {
-            digitalWrite(LEDRED, LOW);
-        } else if (opt == '3') {
-            digitalWrite(LEDYELLOW, HIGH);
-        } else if (opt == '4') {
-            digitalWrite(LEDYELLOW, LOW);
-        } else if (opt == '5') {
-            digitalWrite(LEDGREEN, HIGH);
-        } else if (opt == '6') {
-            digitalWrite(LEDGREEN, LOW);
-        } else if (opt == '7') {
-            digitalWrite(LEDRED, HIGH);
-            digitalWrite(LEDGREEN, HIGH);
-            digitalWrite(LEDYELLOW, HIGH);
-        } else if (opt == '8') {
-            digitalWrite(LEDRED, LOW);
-            digitalWrite(LEDGREEN, LOW);
-            digitalWrite(LEDYELLOW, LOW);
-        } else if (opt == '9') {
-            digitalWrite(LEDYELLOW, HIGH);
-            digitalWrite(LEDGREEN, LOW);
-            digitalWrite(LEDYELLOW, HIGH);
-        } else if (opt == '10') {
-            digitalWrite(LEDRED, LOW);
-            digitalWrite(LEDGREEN, LOW);
-            digitalWrite(LEDYELLOW, LOW);
+        char opt = Serial.read();
+        
+        switch (opt) {
+            case '1':
+                digitalWrite(LEDRED, HIGH);
+                Serial.println("LED RED ON");
+                break;
+            case '2':
+                digitalWrite(LEDRED, LOW);
+                Serial.println("LED RED OFF");
+                break;
+            case '3':
+                digitalWrite(LEDYELLOW, HIGH);
+                Serial.println("LED YELLOW ON");
+                break;
+            case '4':
+                digitalWrite(LEDYELLOW, LOW);
+                Serial.println("LED YELLOW OFF");
+                break;
+            case '5':
+                digitalWrite(LEDGREEN, HIGH);
+                Serial.println("LED GREEN ON");
+                break;
+            case '6':
+                digitalWrite(LEDGREEN, LOW);
+                Serial.println("LED GREEN OFF");
+                break;
+            case '7':
+                digitalWrite(LEDRED, HIGH);
+                digitalWrite(LEDGREEN, HIGH);
+                digitalWrite(LEDYELLOW, HIGH);
+                Serial.println("ALL LEDS ON");
+                break;
+            case '8':
+                digitalWrite(LEDRED, LOW);
+                digitalWrite(LEDGREEN, LOW);
+                digitalWrite(LEDYELLOW, LOW);
+                Serial.println("ALL LEDS OFF");
+                break;
+            case '9':
+                // Blink mode
+                for(int i = 0; i < 5; i++) {  // Blink 5 times
+                    digitalWrite(LEDRED, HIGH);
+                    digitalWrite(LEDGREEN, HIGH);
+                    digitalWrite(LEDYELLOW, HIGH);
+                    delay(500);
+                    digitalWrite(LEDRED, LOW);
+                    digitalWrite(LEDGREEN, LOW);
+                    digitalWrite(LEDYELLOW, LOW);
+                    delay(500);
+                }
+                Serial.println("BLINK MODE TERMINATED");
+                break;
+            case '0':
+                // Exit (turn off all)
+                digitalWrite(LEDRED, LOW);
+                digitalWrite(LEDGREEN, LOW);
+                digitalWrite(LEDYELLOW, LOW);
+                Serial.println("EXIT");
+                break;
+            case 'm':
+                // Show main menu
+                mainMenu();
+                break;
+            default:
+                Serial.println("Invalid option. Press 'm' to show the menu.");
+                break;
         }
+        
+        // Small delay to stabilize reading
+        delay(50);
     }
 }
 
 void mainMenu() {
-  Serial.println("Please select an option: ");
-  Serial.println("Main menu");
-  Serial.println("[1]. Turn on the LED RED");
-  Serial.println("[2]. Turn off the LED RED");
-  Serial.println("[3]. Turn on the LED YELLOW");
-  Serial.println("[4]. Turn off the LED YELLOW");
-  Serial.println("[5]. Turn on the LED GREEN");
-  Serial.println("[6]. Turn off the LED GREEN");
-  Serial.println("[7]. Turn on all");
-  Serial.println("[8]. Turn off all");
-  Serial.println("[9]. Intermitent mode");
-  Serial.println("[10]. Exit");
+    Serial.println("\n::: MENU PRINCIPAL :::");
+    Serial.println("[1]. Encender LED ROJO");
+    Serial.println("[2]. Apagar LED ROJO");
+    Serial.println("[3]. Encender LED AMARILLO");
+    Serial.println("[4]. Apagar LED AMARILLO");
+    Serial.println("[5]. Encender LED VERDE");
+    Serial.println("[6]. Apagar LED VERDE");
+    Serial.println("[7]. Encender TODOS los LEDs");
+    Serial.println("[8]. Apagar TODOS los LEDs");
+    Serial.println("[9]. Modo intermitente");
+    Serial.println("[0]. Salir (apagar todo)");
+    Serial.println("[m]. Mostrar este menú");
+    Serial.print("\nSelecciona una opción: ");
 }
